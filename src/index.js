4c462559db23535119e025ae1ed89666f6fcad9e
Victor Benedict <victorbenedict.bulaong@gmail.com>
2024-04-20 19:28:00 +0800

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import RenderBoard from './RenderBoard';
-import RenderShips from './RenderShips';
-
-import './style.css';
-
-document.body.appendChild(RenderBoard());
-RenderShips();
--- src/player.js
+++
@@ -1,9 +0,0 @@
-import Gameboard from './gameboard';
-
-export default class Player {
-  constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.gameboard = new Gameboard();
-  }
-}
